e7c7a981618cd6676c32984394fa2658c1f8aa30
Tristan Weech <twminebro@gmail.com>
2026-04-29 09:57:55 -0600

    add(Sense) as an Enum

---
+++ src/model/statblock/info/Sense.ts
@@ -0,0 +1,13 @@
+export const Sense = {
+  // Keen Senses (Keen Hearing or Keen Smell = Advantage on Perception checks with that sense))
+  // General Senses
+  PassivePerception: "Passive Perception",
+  Blindsight: "Blindsight",
+  Darkvision: "Darkvision",
+  Tremorsense: "Tremorsense",
+  Truesight: "Truesight",
+
+  // Specific Senses
+  //   WebSense: "WebSense",
+  //   Echolocation: "Echolocation",
+} as const;
