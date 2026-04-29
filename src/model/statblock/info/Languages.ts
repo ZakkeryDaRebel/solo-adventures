c88ff1ac89b600682a186a11ca62cf9f8632441d
Tristan Weech <twminebro@gmail.com>
2026-04-29 09:57:32 -0600

    add(Langauges) as an Enum

---
+++ src/model/statblock/info/Languages.ts
@@ -0,0 +1,25 @@
+export const Languages = {
+  //
+  // Players Handbook Languages
+  //
+
+  // Standard
+  Common: "Common",
+  Dwarvish: "Dwarvish",
+  Elvish: "Elvish",
+  Giant: "Giant",
+  Gnomish: "Gnomish",
+  Goblin: "Goblin",
+  Halfling: "Halfling",
+  Orc: "Orc",
+
+  //Exotic
+  Abyssal: "Abyssal",
+  Celestial: "Celestial",
+  Draconic: "Draconic",
+  DeepSpeech: "Deep Speech",
+  Infernal: "Infernal",
+  Primodial: "Primodial",
+  Sylvan: "Sylvan",
+  Undercommon: "Undercommon",
+} as const;
