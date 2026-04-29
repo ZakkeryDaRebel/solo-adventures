64d19a0dfdad73abfe76a7ee20e5953f50455af5
Tristan Weech <twminebro@gmail.com>
2026-04-29 13:53:40 -0600

    update(Statblock) as required to correctly instantiate a monster

--- src/model/statblock/Statblock.ts
+++ src/model/statblock/Statblock.ts
@@ -6,31 +6,33 @@ import type { Languages } from "./info/Languages";
 import type { Sense } from "./info/Sense";
 import type { Size } from "./info/Size";
 import type { Skills } from "./info/Skills";
+import type { Dice } from "../dice/Dice";
 
 export interface Statblock {
   // Base information
   name: string;
-  size: typeof Size;
-  creatureType: typeof CreatureType;
-  description: string;
-  alignment: typeof Alignment;
+  size: keyof typeof Size;
+  creatureType: keyof typeof CreatureType;
+  descriptionTag: string;
+  alignment: keyof typeof Alignment;
 
   // AC, HP, Speed + Initiative
   armorClass: number;
   hp: {
     max: number;
     cur: number;
+    dice: Dice;
   };
   speed: number;
   initiative: number;
 
   // Ability Scores
   abilityScores: Record<keyof typeof AbilityScores, number>;
-  skills: Record<keyof typeof Skills, number>;
+  skills: Partial<Record<keyof typeof Skills, number>>;
 
   // Traits
-  senses: (typeof Sense)[];
-  languages: (typeof Languages)[];
+  senses: Partial<Record<keyof typeof Sense, number>>;
+  languages: (keyof typeof Languages)[];
   challengeRating: string;
   exp: number;
   //other traits (like passives)
