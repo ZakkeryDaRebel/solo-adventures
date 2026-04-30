1072b998569ae94a93406dc8b82cc1061b1479b3
Tristan Weech <twminebro@gmail.com>
2026-04-30 11:05:24 -0600

    add(Playwright test configurations

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
 
-  roots: ["<rootDir>/tests"],
+  roots: ["<rootDir>/tests/jest"],
 
   moduleNameMapper: {
     "^src/(.*)$": "<rootDir>/src/$1",
