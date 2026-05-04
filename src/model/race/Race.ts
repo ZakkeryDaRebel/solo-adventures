296f9171e0fa4a79f080668afcea69a1a15ea9d2
Tristan Weech <twminebro@gmail.com>
2026-05-04 13:00:48 -0600

    Add(Race) Enum

---
+++ src/model/race/Race.ts
@@ -0,0 +1,16 @@
+export const Race = {
+  // PHB Common Races
+  Dwarf: "Dwarf",
+  Elf: "Elf",
+  Halfling: "Halfling",
+  Human: "Human",
+
+  // PHB Uncommon Races
+  Dragonborn: "Dragonborn",
+  Gnome: "Gnome",
+  HalfElf: "Half Elf",
+  ["Half Elf"]: "HalfElf",
+  HalfOrc: "Half Orc",
+  ["Half Orc"]: "HalfOrc",
+  Tiefling: "Tiefling",
+} as const;
