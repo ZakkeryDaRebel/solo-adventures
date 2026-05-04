3b93d97e0ef1765899c6107963a9e52e3a1e34b3
Tristan Weech <twminebro@gmail.com>
2026-05-04 11:51:28 -0600

    reworked(Main) to add in GameContext

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { GameProvider } from "./context/GameProvider";
 
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
-    <App />
+    <GameProvider>
+      <App />
+    </GameProvider>
   </HashRouter>,
 );
